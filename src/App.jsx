4fa61536b7afda90b9926231f5e83ee2cae70391
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2022-04-18 22:15:07 -0500

    metamask connect using thirdweb

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useAddress, useMetamask } from '@thirdweb-dev/react';
+
 const App = () => {
-  return (
-    <div className="landing">
-      <h1>Welcome to My DAO</h1>
-    </div>
-  );
+	// thirdweb hooks
+	const address = useAddress();
+	const connectWithMetamask = useMetamask();
+	console.log(`Address: ${address}`);
+
+	// Landing page for users that haven't connected their wallets yet.
+	if (!address) {
+		return (
+			<div className='landing'>
+				<h1>Welcome to Woobie DAO</h1>
+				<button onClick={connectWithMetamask} className='btn-hero'>
+					Connect Wallet
+				</button>
+			</div>
+		);
+	}
+
+	// Once wallet is connected page will display below.
+	return (
+		<div className='landing'>
+			<h1>I see you have made it this far.</h1>
+		</div>
+	);
 };
 
 export default App;
